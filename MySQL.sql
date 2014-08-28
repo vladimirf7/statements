cbc03346ad4297ae6745a5f50e9af4b98aa2cd0f
Volodymyr Fesenko <volodymyr.fesenko@barclayscapital.com>
2014-08-28 17:48:10 +0300

    commit 4

--- MySQL.sql
+++ MySQL.sql
@@ -34,29 +34,29 @@ INSERT INTO `item` (item_name, item_category, item_price) VALUES ('Scarf', 3, 1.
 INSERT INTO `item` (item_name, item_category, item_price) VALUES ('Cap', 2, 1.0);
 INSERT INTO `item` (item_name, item_category, item_price) VALUES ('Hat', 2, 1.0);
 
-UPDATE`item`SET item_price = 3.5 WHERE item_id = 1;
+UPDATE `item` SET item_price = 3.5 WHERE item_id = 1;
 
-UPDATE`item`SET item_price = (item_price + item_price / 10);
+UPDATE `item` SET item_price = (item_price + item_price / 10);
 
-DELETE FROM`item`WHERE item_id = 2;
+DELETE FROM `item` WHERE item_id = 2;
 
-SELECT * FROM`item`ORDER BY item_name;
+SELECT * FROM `item` ORDER BY item_name;
 
-SELECT * FROM`item`ORDER BY item_price DESC;
+SELECT * FROM `item` ORDER BY item_price DESC;
 
-SELECT * FROM`item`ORDER BY item_price DESC LIMIT 3;
+SELECT * FROM `item` ORDER BY item_price DESC LIMIT 3;
 
-SELECT * FROM`item`ORDER BY item_price LIMIT 3;
+SELECT * FROM `item` ORDER BY item_price LIMIT 3;
 
-SELECT * FROM`item`ORDER BY item_price DESC LIMIT 3 OFFSET 3;
+SELECT * FROM `item` ORDER BY item_price DESC LIMIT 3 OFFSET 3;
 
-SELECT item_name FROM`item`ORDER BY item_price DESC LIMIT 1;
+SELECT item_name FROM `item` ORDER BY item_price DESC LIMIT 1;
 
-SELECT item_name FROM`item`ORDER BY item_price LIMIT 1;
+SELECT item_name FROM `item` ORDER BY item_price LIMIT 1;
 
 SELECT count(*) FROM item;
 
 SELECT avg(item_price) FROM item;
 
 CREATE VIEW most_expensive AS
-SELECT * FROM`item`ORDER BY item_price DESC LIMIT 3;
+SELECT * FROM `item` ORDER BY item_price DESC LIMIT 3;
